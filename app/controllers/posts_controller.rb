fedc73226c0124406adbecb06c672b4fcdafc958
Dmitry Zuev <mail@dzuev.ru>
2016-02-05 00:14:30 +0300

    jsonapi support

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def create
     response = {test: 'atata'}
     respond_to do |format|
-      format.json { render json: response, status: :created }
+      format.jsonapi { render json: response, status: :created }
     end
   end
 end
