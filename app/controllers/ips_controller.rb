26aeb3c1965b7fba2699d28c7f215a7c68b4d0e0
Dmitry Zuev <mail@dzuev.ru>
2016-02-05 03:13:14 +0300

    Find ips used by different authors

---
+++ app/controllers/ips_controller.rb
@@ -0,0 +1,30 @@
+class IpsController < ApplicationController
+  def index
+    sql = "
+      SELECT *
+      FROM (
+        SELECT posts.ip,
+               COUNT(DISTINCT posts.user_id) as authors
+        FROM posts
+        GROUP BY posts.ip
+      ) d
+      WHERE d.authors > 1
+      ORDER BY d.authors DESC;"
+
+    response = { data: [] }
+
+    ActiveRecord::Base.connection.execute(sql).values.each do |rec|
+      response[:data] << {
+        type: 'ips',
+        id: rec[0],
+        attributes: {
+          authors: User.find(Post.where(ip: rec[0]).pluck(:user_id)).map { |m| m.username }
+        }
+      }
+    end
+
+    respond_to do |format|
+      format.json { render json: response, status: :ok }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
        constraints: { id: /\d+/ }
   get  'posts/top/:amount' => 'posts#top', as: :posts_top, constraints: { amount: /\d+/ }
 
+  get 'ips' => 'ips#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
