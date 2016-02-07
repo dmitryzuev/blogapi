c51ef3330d913b2eab204f32b210e885fc3f812f
Dmitry Zuev <mail@dzuev.ru>
2016-02-08 02:17:05 +0300

    Post model spec

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -4,6 +4,22 @@ FactoryGirl.define do
     content { FFaker::Lorem.paragraph }
     user
     ip { FFaker::Internet.ip_v4_address }
+
+    factory :post_without_title do
+      title nil
+    end
+
+    factory :post_without_content do
+      content nil
+    end
+
+    factory :post_without_user do
+      user nil
+    end
+
+    factory :post_without_ip do
+      ip nil
+    end
   end
 
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:post)                 { FactoryGirl.build(:post) }
+  let(:post_without_title)   { FactoryGirl.build(:post_without_title) }
+  let(:post_without_content) { FactoryGirl.build(:post_without_content) }
+  let(:post_without_user)    { FactoryGirl.build(:post_without_user) }
+  let(:post_without_ip)      { FactoryGirl.build(:post_without_ip) }
+
+  it 'has valid factory' do
+    expect(post).to be_valid
+  end
+
+  it 'is invalid without title' do
+    expect(post_without_title).not_to be_valid
+  end
+
+  it 'is invalid without content' do
+    expect(post_without_content).not_to be_valid
+  end
+
+  it 'is invalid without user' do
+    expect(post_without_user).not_to be_valid
+  end
+
+  it 'is invalid without ip' do
+    expect(post_without_ip).not_to be_valid
+  end
 end
