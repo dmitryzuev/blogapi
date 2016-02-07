6b0f89946198cf128fa663bff7077d92a7d2691a
Dmitry Zuev <mail@dzuev.ru>
2016-02-08 02:23:14 +0300

    Rating model spec

--- spec/factories/ratings.rb
+++ spec/factories/ratings.rb
@@ -2,6 +2,21 @@ FactoryGirl.define do
   factory :rating do
     post
     score { rand(1..5) }
-  end
 
+    factory :rating_greater do
+      score 6
+    end
+
+    factory :rating_less do
+      score 0
+    end
+
+    factory :rating_without_post do
+      post nil
+    end
+
+    factory :rating_without_score do
+      score nil
+    end
+  end
 end
--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Rating, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:rating)               { FactoryGirl.build(:rating) }
+  let(:rating_greater)       { FactoryGirl.build(:rating_greater) }
+  let(:rating_less)          { FactoryGirl.build(:rating_less) }
+  let(:rating_without_post)  { FactoryGirl.build(:rating_without_post) }
+  let(:rating_without_score) { FactoryGirl.build(:rating_without_score) }
+
+  it 'has valid factory' do
+    expect(rating).to be_valid
+  end
+
+  it 'is invalid with rating greater than 5' do
+    expect(rating_greater).not_to be_valid
+  end
+
+  it 'is invalid with rating less than 1' do
+    expect(rating_less).not_to be_valid
+  end
+
+  it 'is invalid without post' do
+    expect(rating_without_post).not_to be_valid
+  end
+
+  it 'is invalid without score' do
+    expect(rating_without_score).not_to be_valid
+  end
 end
