5aa889973367dbc63e9da92929d0774f9a343db9
Dmitry Zuev <mail@dzuev.ru>
2016-02-08 02:11:39 +0300

    IpUsername model spec

--- spec/factories/ip_usernames.rb
+++ spec/factories/ip_usernames.rb
@@ -1,7 +1,14 @@
 FactoryGirl.define do
   factory :ip_username do
-    ip "MyString"
-username "MyString"
-  end
+    username 'user1337'
+    ip '192.168.1.148'
 
+    factory :ip_username_without_username do
+      username nil
+    end
+
+    factory :ip_username_without_ip do
+      ip nil
+    end
+  end
 end
--- spec/models/ip_username_spec.rb
+++ spec/models/ip_username_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe IpUsername, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:ip_username) { FactoryGirl.build(:ip_username) }
+  let(:ip_username_double) { FactoryGirl.build(:ip_username) }
+  let(:ip_username_without_username) { FactoryGirl.build(:ip_username_without_username) }
+  let(:ip_username_without_ip) { FactoryGirl.build(:ip_username_without_ip) }
+
+  it 'has valid factory' do
+    expect(ip_username).to be_valid
+  end
+
+  it 'is invalid without username' do
+    expect(ip_username_without_username).not_to be_valid
+  end
+
+  it 'is invalid without ip' do
+    expect(ip_username_without_ip).not_to be_valid
+  end
+
+  it 'has uniq pairs of ip and username' do
+    ip_username.save!
+    expect(ip_username_double).not_to be_valid
+  end
 end
