755583e92c521b7dd101d9da0496bb684049d552
Dmitry Zuev <mail@dzuev.ru>
2016-02-08 00:25:10 +0300

    Refactor IpsController

--- app/controllers/ips_controller.rb
+++ app/controllers/ips_controller.rb
@@ -1,24 +1,10 @@
 # IpsController
 class IpsController < ApplicationController
   def index
-    ips = {}
-    IpUsername.select(:ip, :username)
-              .where('ip IN (
-                         SELECT ip
-                         FROM ip_usernames
-                         GROUP BY ip
-                         HAVING COUNT(username) > 1
-                      )')
-              .pluck(:ip, :username).each do |row|
-      if ips[row[0]]
-        ips[row[0]] << row[1]
-      else
-        ips[row[0]] = [row[1]]
-      end
-    end
+    res = IpReportGenerator.new.call
 
     respond_to do |format|
-      format.json { render json: { data: ips.to_a.map { |m| { type: 'ips', id: m[0], attributes: { authors: m[1] } } } }, status: :ok }
+      format.json { render json: res[:response], status: res[:status] }
     end
   end
 end
