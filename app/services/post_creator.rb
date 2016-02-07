ee2813ad9fcb86029e67ed9e0e1c5e7cb3415d8a
Dmitry Zuev <mail@dzuev.ru>
2016-02-07 21:17:23 +0300

    PostCreator service object

---
+++ app/services/post_creator.rb
@@ -0,0 +1,39 @@
+# PostCreator service object
+class PostCreator
+  def initialize(params)
+    @params = params
+  end
+
+  def call
+    # TODO: Check input params
+    response = { response: {} }
+    post = Post.new(@params)
+
+    if post.save
+      IpUsernameCacher.new.call(@params[:ip], @params[:username])
+      response[:response] = {
+        data: {
+          type: 'posts',
+          id: post.id,
+          attributes: {
+            title: post.title,
+            username: post.username,
+            ip: post.ip
+          }
+        }
+      }
+      response[:status] = :created
+    else
+      response[:response][:errors] = []
+      post.errors.messages.each do |key, error|
+        response[:response][:errors] << {
+          status: 422, code: key, title: error[0]
+        }
+      end
+      response[:status] = :unprocessable_entity
+    end
+
+    # TODO: return feature-rich object
+    response
+  end
+end
