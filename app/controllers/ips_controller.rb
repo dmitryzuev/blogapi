20fb003d68ca9f83b23990fca8ed89fcbcfb9625
Dmitry Zuev <mail@dzuev.ru>
2016-02-07 10:44:12 +0300

    Optimize IPs fetch query to 70ms average

--- app/controllers/ips_controller.rb
+++ app/controllers/ips_controller.rb
@@ -2,28 +2,24 @@ class IpsController < ApplicationController
   def index
     response = { data: [] }
 
-    data = IpUsername.select(:ip, :username)
-                     .where('ip IN (
-                               SELECT ip
-                               FROM ip_usernames
-                               GROUP BY ip
-                               HAVING COUNT(username) > 1
-                             )').each do |row|
-      if x = response[:data].index { |x| x[:id] == row.ip }
-        response[:data][x][:attributes][:authors] << row.username
+    ips = {}
+    IpUsername.select(:ip, :username)
+              .where('ip IN (
+                         SELECT ip
+                         FROM ip_usernames
+                         GROUP BY ip
+                         HAVING COUNT(username) > 1
+                      )')
+              .pluck(:ip, :username).each do |row|
+      if ips[row[0]]
+        ips[row[0]] << row[1]
       else
-        response[:data] << {
-          type: 'ips',
-          id: row.ip,
-          attributes: {
-            authors: [row.username]
-          }
-        }
+        ips[row[0]] = [row[1]]
       end
     end
 
     respond_to do |format|
-      format.json { render json: response, status: :ok }
+      format.json { render json: ips, status: :ok }
     end
   end
 end
